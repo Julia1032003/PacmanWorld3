776c378c485a587c025972d1945f6d0a1ab74641
Julia <julia1032003@gmail.com>
2019-07-21 17:45:59 +0800

    Initial Commit

---
+++ PacmanWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PacmanWorld
+//
+//  Created by Julia Wang on 2019/7/21.
+//  Copyright © 2019 Julia Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
